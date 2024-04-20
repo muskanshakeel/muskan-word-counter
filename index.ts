b28a5e804e9ffb42f2850a6c69d3e246bc7796f1
muskanshakeel <muskansc25@gmail.com>
2024-04-21 01:00:55 +0500

    pushing word counter to github

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+// Declare a constant answers and assign it to the result of inquirer.prompt function
+
+const answers: {
+    Sentence: string
+} = await inquirer.prompt ([
+    {
+    name: "Sentence",
+    type: "input",
+    message: "Enter your sentenceto count the word: "
+}
+])
+
+const words = answers.Sentence.trim().split(" ")
+
+// print the array of words to the console
+console.log(words)
+
+// print the word count of the Sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
+    
